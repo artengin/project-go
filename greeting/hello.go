9ef6bbb19bbb243b3035778136da063c4bc8963d
artosepyan <artyomosepyan@mail.ru>
2025-05-29 22:10:08 +0300

    Added dependencies

--- greeting/hello.go
+++ greeting/hello.go
@@ -1,7 +1,12 @@
    package greeting
 
    var greeting string = "Golang for Brave!"
+   var bye string = "Bye - Bye!"
 
    func Hello() string {
        return greeting
+   }
+
+   func Bye() string {
+       return bye
    }
--- main.go
+++ main.go
@@ -3,7 +3,9 @@ package main
 import (
   "fmt"
   "project-go/greeting"
+  "github.com/fatih/color"
 )
 func main() {
   fmt.Println(greeting.Hello())
+  color.Cyan(greeting.Bye())
 }
